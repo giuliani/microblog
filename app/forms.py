9bd5461bea75fc919b2820a8179bef7286f8eef2
giuliani perry <perryg.88@gmail.com>
2018-02-15 10:19:15 -0600

    Chapter 10 complete. Email async password reset

--- app/forms.py
+++ app/forms.py
@@ -9,6 +9,16 @@ class LoginForm(FlaskForm):
   remember_me = BooleanField('Remember Me')
   submit = SubmitField('Sign In')
 
+class ResetPasswordRequestForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+  password = PasswordField('Password', validators=[DataRequired()])
+  password2 = PasswordField(
+    'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+  submit = SubmitField('Request Password Reset')
+
 class RegistrationForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired()])
   email = StringField('Email', validators=[DataRequired(), Email()])
